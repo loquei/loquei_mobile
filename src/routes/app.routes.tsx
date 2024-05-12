c6fc0964d3ba66ddc45227d8a474c57ddc61f237
Paulo Cidrão Gomes Torres <103290791+paulo-cidrao@users.noreply.github.com>
2024-05-12 14:36:01 -0300

    refactor - new fold structure

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Home from "@/app/Home";
-import Onboarding from "@/app/Onboarding";
-import SignIn from "@/app/SignIn";
-import SignUp from "@/app/SignUp";
+import Home from "@/screens/Home";
+import Onboarding from "@/screens/Onboarding";
+import SignIn from "@/screens/SignIn";
+import SignUp from "@/screens/SignUp";
 import { theme } from "@/theme";
 
 const { Navigator, Screen } = createNativeStackNavigator();

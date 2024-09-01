beaba196c0b2b17bc33426200ba3d6b4bdb0d102
Vitor <vitor4ntunes@gmail.com>
2024-09-01 13:46:57 -0300

    feat: Add change user photo logic in Profile screen with expo image picker

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,12 +1,20 @@
 import { ScreenHeader } from "@components/ScreenHeader";
 import { gluestackUIConfig } from "@gluestack-ui/config";
-import { Box, Divider, HStack, Heading, Pressable, Text, VStack, View } from "@gluestack-ui/themed";
+import { Box, Divider, HStack, Heading, Image, Pressable, Text, VStack, View } from "@gluestack-ui/themed";
 import { useNavigation } from "@react-navigation/native";
 import { AppNavigatorRoutesProps } from "@routes/app.routes";
 import { Bell, ChevronRight, User, MapPin, LockKeyhole, CircleDollarSign, CircleHelp, Info } from "lucide-react-native";
 import { TouchableOpacity } from "react-native";
 
+import * as ImagePicker from 'expo-image-picker'
+import * as FileSystem from 'expo-file-system'
+import { useState } from "react";
+
 export function Profile() {
+  const [userPhoto, setUserPhoto] = useState(
+    'https://img.freepik.com/free-photo/how-may-i-help-you-smiling-young-modern-guy-with-beard-waiting-looking-hopeful-assisting-standing-white-background_176420-49644.jpg?t=st=1724706288~exp=1724709888~hmac=87b19bdaedc452696bd6888e87642e5ebc9ec98d2d982f5ec3f25b98bd819ebb&w=1380',
+  )
+
   const { tokens } = gluestackUIConfig;
   const navigation = useNavigation<AppNavigatorRoutesProps>();
 
@@ -14,14 +22,46 @@ export function Profile() {
     navigation.navigate('leasing');
   }
 
+  async function handleUserPhotoSelect() {
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      })
+
+      if (photoSelected.canceled) {
+        return
+      }
+
+      const photoUri = photoSelected.assets[0].uri
+
+      if (photoUri) {
+        const photoInfo = (await FileSystem.getInfoAsync(photoUri)) as {
+          size: number
+        }
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+          alert('A imagem selecionada é muito grande, selecione uma imagem de até 5MB')
+          return
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <VStack>
       <ScreenHeader title="Perfil" backButton />
 
       <VStack justifyContent="center" alignItems="center" px={16} mt={16}>
-        <View width={100} height={100} bg="$backgroundLight400" borderRadius="$full" />
+        <Image source={{ uri: userPhoto }} width={96} height={96} rounded={"$full"} borderWidth={1} borderColor="$secondary200" alt="" />
         <Heading color="$textDark800" fontSize="$xl" fontFamily="$heading" textAlign="center">John Doe</Heading>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={handleUserPhotoSelect}>
           <Text color="$teal600" fontSize="$sm" fontFamily="$heading">Trocar foto</Text>
         </TouchableOpacity>
       </VStack>

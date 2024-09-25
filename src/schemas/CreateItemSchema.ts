592fc7347f6fba45de29e675041b24230cd39555
Paulo Cidrão <paulocdrao.123@gmail.com>
2024-09-24 23:28:40 -0300

    add: interface values

---
+++ src/schemas/CreateItemSchema.ts
@@ -0,0 +1,10 @@
+import * as y from "yup";
+
+export const PostItemSchema = y.object({
+  name: y.string().required().trim(),
+  description: y.string().required().trim(),
+  daily_value: y.number().required().positive(),
+  max_days: y.number().required().positive(),
+  min_days: y.number().required().positive(),
+  categories_id: y.string().required().trim(),
+})

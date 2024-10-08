52847c09cd183ce1bbd1954d0217a5efcd4aeba6
Paulo Cidrão <paulocdrao.123@gmail.com>
2024-10-08 00:27:35 -0300

    fix post user

--- src/api/PostUser.ts
+++ src/api/PostUser.ts
@@ -3,7 +3,6 @@ import { IPostUser } from "../@types/TUserData"
 import { api } from "./axios/axiosConfig"
 
 export const create_user = async (data: IPostUser) => {
-  console.log(api + '/users')
   const headers = { 'Content-Type': 'application/json' }
   try {
     await api.post('/users', JSON.stringify(data), { headers })

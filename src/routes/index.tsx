0742ceecc00761095475a38d22076bed2c98b9fc
Vitor Hugo <vitor4ntunes@gmail.com>
2024-10-29 23:26:09 -0300

    feat: Checks user authentication and sends them to the correct route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,32 @@
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
 import { AuthRoutes } from "./auth.routes";
+import { AppRoutes } from "./app.routes";
 import { gluestackUIConfig } from "../../config/gluestack-ui.config";
 import { Box } from "@gluestack-ui/themed";
+import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export function Routes() {
   const theme = DefaultTheme;
   theme.colors.background = gluestackUIConfig.tokens.colors.backgroundLight50;
 
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      const authToken = await AsyncStorage.getItem("AuthToken");
+      setIsUserAuthenticated(!!authToken);
+      console.log("authToken", authToken);
+    };
+
+    checkAuthentication();
+  }, []);
+
   return (
     <Box flex={1} bg="$backgroundLight50" bgColor="$backgroundLight50">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        {isUserAuthenticated ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   );

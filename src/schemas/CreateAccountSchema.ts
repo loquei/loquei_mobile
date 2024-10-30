2a79014e653c84cdcea2df13ea7d18568685b3d9
Vitor Hugo <vitor4ntunes@gmail.com>
2024-10-29 23:36:03 -0300

    chore(schemas): adjust user schema for API integration and fields validations

--- src/schemas/CreateAccountSchema.ts
+++ src/schemas/CreateAccountSchema.ts
@@ -1,12 +1,28 @@
 import * as y from "yup";
 
-
 export const CreateAccountSchema = y.object({
-  username: y.string().required().trim().matches(/^[a-zA-ZçÇ\s]+$/),
-  personal_name: y.string().required().trim(),
-  email: y.string().required().email().trim(),
-  phone: y.string().required().trim().matches(/^\d{8,14}$/),
-  document: y.string().required().trim(),
-  birth: y.string().required().trim(),
-
-})
+  personal_name: y.string().required("O nome é obrigatório"),
+  username: y.string().required("O nome de usuário é obrigatório"),
+  email: y
+    .string()
+    .required("O email é obrigatório")
+    .matches(
+      /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
+      "Digite um email válido"
+    ),
+  document: y
+    .string()
+    .required("O CPF é obrigatório")
+    .matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, "Digite um CPF válido"),
+  birth: y
+    .string()
+    .required("A data de nascimento é obrigatória")
+    .matches(
+      /^\d{2}\/\d{2}\/\d{4}$/,
+      "A data de nascimento deve estar no formato DD/MM/AAAA"
+    ),
+  phone: y
+    .string()
+    .required("O telefone é obrigatório")
+    .min(11, "Digite um telefone válido"),
+});
--- src/schemas/SendCodeSchema.ts
+++ src/schemas/SendCodeSchema.ts
@@ -1,5 +1,7 @@
 import * as y from "yup";
 
 export const SendCodeSchema = y.object({
-  code: y.string().required().trim()
+  code: y.string().required(
+    "O código é obrigatório"
+  ).trim().length(6, "O código deve ter 6 dígitos"),
 });

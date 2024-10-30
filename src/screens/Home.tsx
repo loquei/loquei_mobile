2f12b7b9a3fc4b3884ab5edf69c2c55b635f2510
Vitor Hugo <vitor4ntunes@gmail.com>
2024-10-29 23:23:10 -0300

    feat: adds product data dynamically from the API
    - Check user authentication

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,14 +2,17 @@ import { CategoryCard } from "@components/CategoryCard";
 import { Tag } from "@components/Tag";
 import { ProductCard } from "@components/ProductCard";
 import { ScreenHeader } from "@components/ScreenHeader";
-import { HStack, VStack, Text, Pressable } from "@gluestack-ui/themed";
+import { HStack, VStack, Text, Pressable, get } from "@gluestack-ui/themed";
 
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { AppNavigatorRoutesProps } from "@routes/app.routes";
-import { useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { FlatList, ScrollView, SectionList } from "react-native";
 import { ListItems } from "../api/listItem";
 import { IGetItem } from "../@types/TItem";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { GetItemFirstImage } from "../api/getItemFirstImage";
+import { getUser } from "../api/getUser";
 
 export function Home() {
   const [itemData, setItemData] = useState<IGetItem[]>([]);
@@ -21,15 +24,56 @@ export function Home() {
     "Carros",
   ]);
   const [categorySelected, setCategorySelected] = useState("");
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await ListItems();
-      if (data) {
-        setItemData(data);
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState(false);
+  const [currentUser, setCurrentUser] = useState<any>();
+
+  const baseURL = "http://192.168.3.2:8080";
+
+  useFocusEffect(
+    useCallback(() => {
+      const fetchData = async () => {
+        try {
+          const data = await ListItems();
+          if (data) {
+            setItemData(data);
+            console.log("data", data);
+            if (data.length > 0) {
+              console.log("First item image link", baseURL + data[0].images.links[0]);
+            }
+          }
+        } catch (error) {
+          console.error("Error fetching data: ", error);
+        }
+      };
+
+      const checkAuthentication = async () => {
+        const authToken = await AsyncStorage.getItem("AuthToken");
+        console.log("authToken", authToken);
+        setIsUserAuthenticated(!!authToken);
+      };
+
+      const fetchCurrentUser = async () => {
+        try {
+          const currentUser = await getUser();
+          setCurrentUser(currentUser);
+          await AsyncStorage.setItem("currentUser", JSON.stringify(currentUser));
+          console.log("currentUser", currentUser);
+        } catch (error) {
+          console.error("Error fetching current user:", error);
+        }
+      };
+
+      const fetchFirstImage = async () => {
+        const response = await GetItemFirstImage("3b883edcb3024669befb171954b60334");
+        console.log("First image response", response);
       }
-    };
-    fetchData();
-  }, []);
+
+      fetchData();
+      checkAuthentication();
+      fetchCurrentUser();
+      fetchFirstImage();
+    }, [])
+  );
 
   return (
     <ScrollView
@@ -39,6 +83,14 @@ export function Home() {
     >
       <VStack>
         <ScreenHeader title="São Paulo, SP" iconButton />
+
+        {isUserAuthenticated && (
+          <Text fontFamily="$heading" fontSize="$lg" color="$textDark800" px={16} mt={16}>
+            {
+              currentUser ? `Olá, ${currentUser.items[0].personal_name}` : "Olá, usuário"
+            }
+          </Text>
+        )}
 
         <FlatList
           data={categories}
@@ -56,124 +108,69 @@ export function Home() {
           style={{ marginTop: 16, maxHeight: 44, minHeight: 44 }}
         />
 
-        <SectionList
-          scrollEnabled={false}
-          sections={[{ title: "Produtos Principais", data: itemData }]}
-          keyExtractor={(item) => item.id}
-          renderItem={(item) => null}
-          renderSectionHeader={({ section }) => (
-            <VStack>
-              <Text
-                px={16}
-                py={8}
-                fontFamily="$heading"
-                fontSize="$lg"
-                color="$textDark800"
-              >
-                {section.title}
-              </Text>
-              <FlatList
-                data={section.data || []}
-                keyExtractor={(item) => item.id}
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                renderItem={({ item }) => (
-                  <Pressable
-                    onPress={() => navigation.navigate("productDetails")}
+        {itemData ? (
+          itemData.length > 0 ? (
+            <SectionList
+              scrollEnabled={false}
+              sections={[
+                {
+                  title: "Produtos Principais",
+                  data: itemData.map(item => ({
+                    id: item.id,
+                    title: item.name,
+                    rating: 5,
+                    ratingCount: 5,
+                    price: item.daily_value.toFixed(2).replace('.', ','),
+                    discountPrice: item.daily_value.toFixed(2).replace('.', ','),
+                    images: item.images,
+                  })),
+                },
+              ]}
+              keyExtractor={(item) => item.id}
+              renderItem={({ item }) => null}
+              renderSectionHeader={({ section }) => (
+                <VStack>
+                  <Text
+                    px={16}
+                    py={8}
+                    fontFamily="$heading"
+                    fontSize="$lg"
+                    color="$textDark800"
                   >
-                    <ProductCard
-                      title={item.name}
-                      price={item.daily_value}
-                      discountPrice={item.daily_value - 10}
-                      rating={5}
-                      ratingCount={5}
-                    />
-                  </Pressable>
-                )}
-                contentContainerStyle={{ paddingHorizontal: 16 }}
-                ItemSeparatorComponent={() => <HStack width={12} />}
-              />
-            </VStack>
-          )}
-        ></SectionList>
+                    {section.title}
+                  </Text>
+                  <FlatList
+                    data={section.data || []}
+                    keyExtractor={(item) => item.id}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    renderItem={({ item }) => (
+                      <Pressable
+                        onPress={() => navigation.navigate("productDetails", { id: item.id })}
+                      >
+                        <ProductCard
+                          imagePath={item.images && item.images.links.length > 0 ? baseURL + item.images.links[0] : ''}
+                          title={item.title}
+                          price={item.price}
+                          discountPrice={item.discountPrice}
+                          rating={item.rating}
+                          ratingCount={item.ratingCount}
+                        />
+                      </Pressable>
+                    )}
+                    contentContainerStyle={{ paddingHorizontal: 16 }}
+                    ItemSeparatorComponent={() => <HStack width={12} />}
+                  />
+                </VStack>
+              )}
+            />
+          ) : null
+        ) : (
+          <Text fontFamily="$heading" fontSize="$lg" color="$textDark800" px={16} mt={16}>
+            Carregando...
+          </Text>
+        )}
 
-        <SectionList
-          scrollEnabled={false}
-          sections={[
-            {
-              title: "Novos",
-              data: [
-                {
-                  id: "1",
-                  title: "Smartphone Samsung Galaxy S21",
-                  rating: 4,
-                  ratingCount: 14,
-                  price: "R$ 3.999,00",
-                  discountPrice: "R$ 3.499,00",
-                },
-                {
-                  id: "2",
-                  title: "Smartwatch Samsung Galaxy Watch 4",
-                  rating: 4,
-                  ratingCount: 14,
-                  price: "R$ 3.999,00",
-                  discountPrice: "R$ 3.499,00",
-                },
-                {
-                  id: "3",
-                  title: "Notebook Dell Inspiron 15",
-                  rating: 4,
-                  ratingCount: 14,
-                  price: "R$ 3.999,00",
-                  discountPrice: "R$ 3.499,00",
-                },
-                {
-                  id: "4",
-                  title: 'Smart TV LG 50" 4K',
-                  rating: 4,
-                  ratingCount: 14,
-                  price: "R$ 3.999,00",
-                  discountPrice: "R$ 3.499,00",
-                },
-              ],
-            },
-          ]}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => null} // Rendered in renderSectionHeader
-          renderSectionHeader={({ section }) => (
-            <VStack>
-              <Text
-                px={16}
-                py={8}
-                fontFamily="$heading"
-                fontSize="$lg"
-                color="$textDark800"
-              >
-                {section.title}
-              </Text>
-              <FlatList
-                data={section.data || []}
-                keyExtractor={(item) => item.id}
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                renderItem={({ item }) => (
-                  <Pressable
-                    onPress={() => navigation.navigate("productDetails")}
-                  >
-                    <ProductCard
-                      title={item.title}
-                      rating={item.rating}
-                      ratingCount={item.ratingCount}
-                    />
-                  </Pressable>
-                )}
-                contentContainerStyle={{ paddingHorizontal: 16 }}
-                ItemSeparatorComponent={() => <HStack width={12} />}
-              />
-            </VStack>
-          )}
-          style={{ marginTop: 16 }}
-        />
 
         <SectionList
           scrollEnabled={false}

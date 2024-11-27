22eae68f9bbc52848ad81fe50d8fe5030a8ded9a
Vitor Hugo <vitor4ntunes@gmail.com>
2024-11-27 16:00:08 -0300

    chore: Remove unnecessary files and folders

--- app/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components/native"
-import { theme } from "@/theme";
-export const Container = styled.View`
- flex: 1;
-  background-color:${theme.colors.gray50};
-  margin-top: 10;
-`;
